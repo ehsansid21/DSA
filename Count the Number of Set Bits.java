e3dc26a043fff2f3dacd681c40fa72fa19e86ad0
Ehsan Siddiqui <ehsansid214@gmail.com>
2025-10-18 14:34:28 +0530

    Day 2: Count Set Bits problem

---
+++ Count the Number of Set Bits.java
@@ -0,0 +1,20 @@
+ class Solution {
+
+    public int countSetBits(int n) {
+        int c = 0;
+        while (n > 0) {
+            n = n & (n - 1);
+            c++;
+        }
+        return c;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int n = 29;
+        int result = sol.countSetBits(n);
+
+        System.out.println("Number of set bits in " + n + " is: " + result);
+    }
+}
