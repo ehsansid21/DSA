850ad2b2a98601b58a3bfd0fc200aa44a9d6536c
ehsansid21 <ehsansid6357@gmail.com>
2026-08-17 01:59:02 +0530

    Time: 27 ms (24.90%) | Memory: 139.2 MB (85.67%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.java
+++ 523-continuous-subarray-sum/continuous-subarray-sum.java
@@ -2,8 +2,6 @@ class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
 
         Map<Integer, Integer> map = new HashMap<>();
-
-        // remainder 0 exists before the array starts
         map.put(0, -1);
 
         int prefix = 0;
@@ -19,14 +17,11 @@ class Solution {
             }
 
             if (map.containsKey(rem)) {
-
-                // subarray must contain at least 2 elements
                 if (i - map.get(rem) >= 2) {
                     return true;
                 }
 
             } else {
-                // Store only the FIRST occurrence
                 map.put(rem, i);
             }
         }
